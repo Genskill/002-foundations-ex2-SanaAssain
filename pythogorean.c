c94ac4b29aa3b0f807de0c5878eb7b49381681d1
SanaAssain <84311933+SanaAssain@users.noreply.github.com>
2021-05-21 00:26:47 +0530

    Update pythogorean.c

--- pythogorean.c
+++ pythogorean.c
@@ -1,57 +1,31 @@
-#include<stdio.h>
+//pythagorean.c
 
+#include<stdio.h>
 #include<cs50.h>
 
-
-
-int main()
-{
-
-   int a= get_int(" Side 1:");
-
-   int b= get_int(" Side 2:");
-   
+int main(void){
+int a= get_int(" Side 1:");
+int b= get_int(" Side 2:");
 int c= get_int(" Side 3:");
-   
 
-if(a>b && a>c)
-   {
-
-      if (b*b + c*c == a*a)
-     
-         printf("Yes\n");
-
-      else
-
-         printf("No\n");
- 
+if(a>b && a>c){
+    if(b*b + c*c == a*a)
+      printf("Yes\n");
+    else
+      printf("No\n");
 
 
-  }
-  
- else if (b>a && b>c)
-   {
-
-      if(a*a +c*c == b*b)
-
-         printf("Yes\n");
-
-      else
-
-         printf("No\n");
-  
+  }    
+ else if(b>a && b>c){
+    if(a*a +c*c == b*b)
+       printf("Yes\n");
+    else
+       printf("No\n");            
  }
-
-   else if (c>a && c>b)
-   {
-      if (b*b + a*a == c*c)
-
-         printf("Yes\n");
-
-      else
-
-         printf("No\n");
-
-   }
-
+ else if(c>a && c>b){
+    if(b*b + a*a == c*c)
+      printf("Yes\n");
+    else
+      printf("No\n");
+ }
 }
