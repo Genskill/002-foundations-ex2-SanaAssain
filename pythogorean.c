c6a0890949d75d9d29a32491a5091f168050714c
SanaAssain <84311933+SanaAssain@users.noreply.github.com>
2021-05-21 03:46:58 -0700

    Update pythogorean.c

--- pythogorean.c
+++ pythogorean.c
@@ -4,28 +4,28 @@
 #include<cs50.h>
 
 int main(void){
-float a= get_float("side 1 ");
-float b= get_float("\nside 2 ");
-float c= get_float("\nside 3 ");
+int a= get_int(" Side 1:");
+int b= get_int(" Side 2:");
+float c= get_int(" Side 3:");
 
 if(a>b && a>c){
     if(b*b + c*c == a*a)
-      printf("\nYes");
+      printf("Yes\n");
     else
-      printf("\nNo");
+      printf("No\n");
 
 
   }    
  else if(b>a && b>c){
     if(a*a +c*c == b*b)
-       printf("\nYes");
+       printf("Yes\n");
     else
-       printf("\nNo");            
+       printf("No\n");            
  }
  else if(c>a && c>b){
     if(b*b + a*a == c*c)
-      printf("\nYes");
+      printf("Yes\n");
     else
-      printf("\nNo");
+      printf("No\n");
  }
 }
